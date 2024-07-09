44faade896e62b4f315972021372f2e906b52240
moeezali <moeezali2375@gmail.com>
2024-07-09 18:31:43 +0500

    Fix Cors Issue

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,30 @@
-const express=require("express");
-const connectDB=require("./config/db");
-const todoRoutes=require("./routes/todo");
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const todoRoutes = require("./routes/todo");
 
 require("dotenv").config();
 
-const app=express();
-const PORT=process.env.PORT
+const app = express();
+const PORT = process.env.PORT;
 connectDB();
 
 //! Middlewares
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //! Routes
-app.use("/todo",todoRoutes);
+app.use("/todo", todoRoutes);
 
-app.get("*",async (req,res)=>{
-    res.redirect("/todo");
-})
+app.get("*", async (req, res) => {
+  res.redirect("/todo");
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server started on Port: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server started on Port: ${PORT}`);
+});
